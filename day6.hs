8cbf7a536b602806e331ac2781d70e7de201ed27
Edvard Hübinette <edvard@hubinette.me>
2015-12-13 12:41:01 +0100

    Added day 6, takes years

---
+++ day6.hs
@@ -0,0 +1,66 @@
+module Main where
+import Data.List.Split
+
+off :: Int -> Int
+off _ = 0
+
+on :: Int -> Int
+on _ = 1
+
+toggle :: Int -> Int
+toggle 1 = 0
+toggle 0 = 1
+
+s2f :: String -> Int -> Int
+s2f s = case s of
+            "on" -> on
+            "off" -> off
+            "toggle" -> off
+
+
+-- Takes two coordinates and a grid, applies 
+-- function to the specified rectangle
+-- and returns the resulting grid
+applyGrid :: (Int -> Int) -> [(Int,Int)] -> [[Int]] -> [[Int]]
+applyGrid f [(x1,y1),(x2,y2)] g = [[apply (x,y) | y <- [0..999]] | x <- [0..999]]
+    where 
+        rekt = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
+        apply (x,y) = if (x,y) `elem` rekt then f $ (g !! x) !! y
+                                           else (g !! x) !! y
+
+-- Takes "x,y" and returns (x,y) shamelessly.
+createPoint :: String -> (Int,Int)
+createPoint s = (head pair, last pair)
+    where pair = map (read :: String -> Int) (splitOn "," s)
+
+
+applyRec :: [[String]] -> [[Int]] -> [[Int]]
+applyRec [x]    g = applyGrid (s2f $ head x) 
+                              [createPoint $ x !! 1, createPoint $ x !! 2] 
+                              g
+applyRec (x:xs) g = applyRec xs (applyRec [x] g)
+
+clean :: String -> [String]
+clean ('t':'u':'r':'n':' ':s) = clean s
+clean s                       = [head parts, parts !! 1, parts !! 3]
+    where parts = splitOn " " s
+
+{--
+main :: IO ()
+main = do
+    content <- fmap lines (readFile "input6.txt")
+    let input = map clean content
+    let grid = applyRec [head input] [[0 | x <- [0..999]] | y <- [0..999]]
+    let countOn = sum $ map sum grid
+    putStrLn $ "Total lights on: " ++ show countOn
+
+--}
+
+main :: IO ()
+main = do
+    content <- fmap lines (readFile "input6.txt")
+    let input = map clean content
+    --let grid = applyRec [head input] [[0 | x <- [0..999]] | y <- [0..999]]
+    let grid = applyGrid on [(489,959), (759,964)] [[0 | x <- [0..999]] | y <- [0..999]]
+    let countOn = sum $ map sum grid
+    putStrLn $ "Total lights on: " ++ show countOn
